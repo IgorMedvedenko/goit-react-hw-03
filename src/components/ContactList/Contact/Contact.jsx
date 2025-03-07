5dc6504b0aa0a1a2f122f47d489da77293e7dcbe
IgorMedvedenko <noveciti0@gmail.com>
2025-03-08 00:46:44 +0200

    fixcss

--- src/components/ContactList/Contact/Contact.jsx
+++ src/components/ContactList/Contact/Contact.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { PiPhoneFill } from "react-icons/pi";
+import { RiUserAddFill } from "react-icons/ri";
 import styles from "./Contact.module.css";
 
 export default function Contact({ id, name, number, onDeleteContact }) {
   return (
     <li className={styles.item}>
       <p className={styles.text}>
-        {name}: {number}
+        <div className={styles.contHolder}>
+          <RiUserAddFill className={styles.icon} /> {name}
+        </div>
+        <div className={styles.contHolder}>
+          <PiPhoneFill className={styles.icon} /> {number}
+        </div>
       </p>
       <button
         type="button"
